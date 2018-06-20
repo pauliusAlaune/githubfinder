d847b6b3d9261f50c5fa505e2da4af5b6cec2c1a
pauliusAlaune <paulius.alaune@gmail.com>
2018-06-20 22:14:28 +0300

    show alert massage if user not exist

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ searchUser.addEventListener('keyup', (e) => {
         .then(data => {
             if(data.profile.message === 'Not Found'){
                // show alert
+                ui.showAlert('User not found', 'alert alert-danger');
             } else {
                // show profile 
                 ui.showProfile(data.profile);
@@ -21,5 +22,6 @@ searchUser.addEventListener('keyup', (e) => {
         })
     } else {
         // clear profile
+        ui.clearProfile();
     }
 });
--- ui.js
+++ ui.js
@@ -2,6 +2,7 @@ class UI {
     constructor() {
         this.profile = document.getElementById('profile');
     }
+    // display profile in ui
     showProfile(user){
         this.profile.innerHTML = `
             <div class="card card-body mb-3">
@@ -28,5 +29,38 @@ class UI {
             <h3 class="page-heading mb-3">Latest Repos</h3>
             <div id="repos"></div>
         `;
+    }
+    // show alert message
+    showAlert(message, className){
+        // clear any remaining alerts
+        this.clearAlert();
+        // create div
+        const div = document.createElement('div');
+        // add classes
+        div.className = className;
+        // add text 
+        div.appendChild(document.createTextNode(message));
+        // get parent
+        const container = document.querySelector('.searchContainer');
+        // get search box
+        const search = document.querySelector('.search');
+        // insert alert
+        container.insertBefore(div, search);
+        // timeout after 3s
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+    // CLEAR ALERT MESSAGE
+    clearAlert(){
+        const currentAlert = document.querySelector('.alert');
+        if(currentAlert){
+            currentAlert.remove();
+        }
+    }
+    
+    // clear profile
+    clearProfile(){
+        this.profile.innerHTML = '';
     }
 }
