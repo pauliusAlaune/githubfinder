1d1497d7fcb9a4e431f833f70bf3cf735b0d989f
pauliusAlaune <paulius.alaune@gmail.com>
2018-06-21 20:55:35 +0300

    display repos & fetch

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ searchUser.addEventListener('keyup', (e) => {
             } else {
                // show profile 
                 ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
             }
         })
     } else {
--- github.js
+++ github.js
@@ -2,15 +2,22 @@ class GitHub {
     constructor(){
         this.client_id = '6f6d944edfe154261ec9';
         this.client_secret = '6f99629144db195bb55c02db7a7daaa0d0658a85';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
     
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
         
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        
         const profileData = await profileResponse.json();
         
+        const repos = await repoResponse.json();
+        
         return {
-            profile: profileData
+            profile: profileData,
+            repos
         }
     }
 }
